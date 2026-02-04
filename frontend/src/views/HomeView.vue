1024e71d4a6f6c6c5ac37f8663c1300d2e99dfc7
inacio gabriel <inaciogabrieltavaresdasilva@gmail.com>
2026-02-04 14:36:13 -0300

    adiciona o componente HomeView.vue com a estrutura inicial

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template >
+     <HomePage/>
+</template>
+<script>
+import HomePage from '../components/HomePage.vue';
+
+export default {
+    components: {
+        HomePage
+    }
+}
+</script>
+<style lang="">
+    
+</style>
