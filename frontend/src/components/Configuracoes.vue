936fa446dfc7d0b020277cec6614ec3361c392bf
inacio gabriel <inaciogabrieltavaresdasilva@gmail.com>
2026-04-24 01:59:21 -0300

    remoçao de um <p> do componenete de configuraões

--- frontend/src/components/Configuracoes.vue
+++ frontend/src/components/Configuracoes.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="section text-center">
     <h1 class="h1SS">Configurações</h1>
-    <p>Gerencie as configurações do seu sistema escolar aqui.</p>
+   
   </v-container>
 </template>
 
